8f0a18c520a4970bd895a91c3634a50676428eb0
ffpaulzhao <yz496@cam.ac.uk>
2018-05-27 21:25:20 +0100

    debug test_names

--- test_names.py
+++ test_names.py
@@ -8,9 +8,15 @@ def new_names():
     new_names = Names()
     return new_names
 
+def new_names_with_items():
+    '''returns an instance of class Names with items '''
+    new_names_with_items = Names()
+    new_names.lookup(['DEVICE', 'CONNECT', 'MONITOR', 'CLOCK', 'SWITCH', 'AND', 'NAND', 'G2')
+    return new_names
+
 def test_unique_error_code(new_names):
-    assert new_names.unique_error_code(3) == [0,1,2]
-    assert new_names.unique_error_code(2) == [3,4]
+    assert new_names.unique_error_codes(3) == [0,1,2]
+    assert new_names.unique_error_codes(2) == [3,4]
     assert new_names.error_code_count == 5
 
 
@@ -25,13 +31,13 @@ def test_lookup(new_names):
     assert new_names.lookup(['CLOCK','AND','SWITCH','NAND']) == [3,5,4,6]
     assert new_names.lookup('G2') == 7
 
-def test_query(new_names):
+def test_query(new_names_with_items):
     assert new_names.query('DEVICE') == 0
     assert new_names.query('device') == None
     assert new_names.query('AND') == 5
     assert new_names.query('G1') == None
 
-def test_get_name_string(new_names):
+def test_get_name_string(new_names_with_items):
     assert new_names.get_name_string(0) == 'DEVICE'
     assert new_names.get_name_string(3) == 'CLOCK'
     assert new_names.get_name_string(12) == None
